9c6c3730bb978268ed0c278848713a0a1fb1d037
Julian Fernandez <fernandezjuulian@gmail.com>
2022-05-20 19:18:37 -0300

    borro comentarios, el resto se mantiene igual. Solo limpié el código

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,12 +2,10 @@
 
 const Item = ({id, title, price, pictureURL}) => {
     return (
-        <div className="ItemCard">
-            
+        <div className="ItemCard">            
                 <h1>{id}. {title}</h1>                
                 <img src={pictureURL} alt='imagen'></img>
-                <h3>${price}</h3>            
-            
+                <h3>${price}</h3>                       
         </div>
     )
 }
