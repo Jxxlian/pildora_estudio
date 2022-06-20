8037e25abcfb93ed288c3992e771d9a306e15986
Julian Fernandez <fernandezjuulian@gmail.com>
2022-06-20 14:29:25 -0300

    error con retorno del promise del navbar

--- src/service/firebase/index.js
+++ src/service/firebase/index.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBUxHmMxRACTwtsYiHThta9sUBYtnDcAU",
-  authDomain: "bd-pildora-estudio.firebaseapp.com",
-  projectId: "bd-pildora-estudio",
-  storageBucket: "bd-pildora-estudio.appspot.com",
-  messagingSenderId: "832367778007",
-  appId: "1:832367778007:web:833a2112ada11c0af8f892"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
